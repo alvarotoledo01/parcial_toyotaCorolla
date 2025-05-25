fdeed2baa0160ffc998573da7e9fe176eefe1751
toviazs <tobias.pazposse@alu.frt.utn.edu.ar>
2025-05-24 23:31:33 -0300

    feat(eda): add eda_report function for exploratory data analysis and visualization

---
+++ dagster_project/assets/eda.py
@@ -0,0 +1,79 @@
+from dagster import asset
+import mlflow
+import pandas as pd
+import os
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+@asset(deps=["preprocess_data", "setup_mlflow"])
+def eda_report(context, preprocess_data: pd.DataFrame, setup_mlflow: str):
+    df = preprocess_data.copy()
+    eda_folder = os.path.join("mlartifacts", "eda")
+    os.makedirs(eda_folder, exist_ok=True)
+
+    # df describe
+    describe_path = os.path.join(eda_folder, "df_describe.csv")
+    df.describe().to_csv(describe_path)
+
+    # corelation matrix
+    correlation_path = os.path.join(eda_folder, "correlation_matrix.png")
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(
+        df.corr(),
+        annot=True,
+        fmt=".2f",
+        cmap="coolwarm",
+        cbar_kws={"shrink": 0.75},
+        linewidths=0.5,
+        annot_kws={"size": 10},
+    )
+    plt.title("Correlation Matrix")
+    plt.savefig(correlation_path)
+    plt.close()
+
+    # pairplot
+    pairplot_path = os.path.join(eda_folder, "pairplot.png")
+    sns.pairplot(df, diag_kind="kde").savefig(pairplot_path)
+    plt.close()
+
+    # feature plots
+    numerical_features = df.select_dtypes(include=["int64", "float64"]).columns
+    features_folder = os.path.join(eda_folder, "features")
+    os.makedirs(features_folder, exist_ok=True)
+
+    feature_plots = []
+    for feature in numerical_features:
+        plt.figure(figsize=(14, 10))
+
+        plt.subplot(2, 2, 1)
+        sns.boxplot(x=df[feature])
+        plt.title(f"Box Plot of {feature}")
+
+        plt.subplot(2, 2, 2)
+        sns.histplot(df[feature], kde=True)
+        plt.title(f"Histogram of {feature}")
+
+        plt.subplot(2, 2, 3)
+        sns.violinplot(y=df[feature])
+        plt.title(f"Violin Plot of {feature}")
+
+        plt.subplot(2, 2, 4)
+        sns.kdeplot(df[feature], fill=True)
+        plt.title(f"Density Plot of {feature}")
+
+        plt.tight_layout()
+
+        feature_plot_path = os.path.join(features_folder, f"{feature}_plots.png")
+        plt.savefig(feature_plot_path)
+        plt.close()
+
+        feature_plots.append(feature_plot_path)
+
+    with mlflow.start_run(run_id=setup_mlflow):
+        context.log.info(f"Logging EDA artifacts")
+        mlflow.log_artifact(describe_path, "eda")
+        mlflow.log_artifact(correlation_path, "eda")
+        mlflow.log_artifact(pairplot_path, "eda")
+        for feature_plot in feature_plots:
+            mlflow.log_artifact(feature_plot, "eda")
--- dagster_project/definitions.py
+++ dagster_project/definitions.py
@@ -1,8 +1,8 @@
 from dagster import Definitions, load_assets_from_modules
 
-from dagster_project.assets import load_data, preprocessing, setup  # noqa: TID252
+from dagster_project.assets import eda, load_data, preprocessing, setup  # noqa: TID252
 
-all_assets = load_assets_from_modules([load_data, setup, preprocessing])
+all_assets = load_assets_from_modules([load_data, setup, preprocessing, eda])
 
 defs = Definitions(
     assets=all_assets,
